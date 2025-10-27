bcac74fbe77b72396bbeeb4a1add124520d353e2
hiIgooTgar <igotegarprambudhy128945@gmail.com>
2025-10-27 22:35:20 +0700

    Add modul 5 SBD - DCL and Aggregasi and Updated DML

--- praktikum_5/praktikum_4_ampu_sport_dcl_agregat.sql
+++ praktikum_5/praktikum_4_ampu_sport_dcl_agregat.sql
@@ -2,7 +2,6 @@ USE mysql;
 SHOW TABLES;
 
 /* Perintah DCL - GRANT dan REVOKE */
-
 SELECT USER, HOST, PASSWORD FROM USER
 
 CREATE USER 'user_admin'@'localhost';
@@ -27,16 +26,9 @@ GRANT SELECT (nama, harga, stok) ON 24sa11a159_ampu_sport.tbproduk
 TO 'member'@'localhost';
 
 
-SHOW GRANTS FOR kasir1@localhost;
 SHOW GRANTS FOR 'kasir1'@'localhost';
-
-SHOW GRANTS FOR MEMBER@localhost;
 SHOW GRANTS FOR 'member'@'localhost';
-
-SHOW GRANTS FOR ADMIN@localhost;
 SHOW GRANTS FOR 'admin'@'localhost';
-
-SHOW GRANTS FOR OWNER@localhost;
 SHOW GRANTS FOR 'owner'@'localhost';
 
 
@@ -49,7 +41,6 @@ REVOKE SELECT ON 24sa11a159_ampu_sport.* FROM 'user_admin'@'localhost';
 
 
 /* Fungsi Aggregasi */
-
 SELECT COUNT(*) AS 'Jumlah Member' FROM tbmember
 
 SELECT alamat, COUNT(namamember) AS 'Jumlah Member' FROM tbmember
@@ -62,3 +53,60 @@ GROUP BY idkategori
 
 SELECT idkategori, SUM(stok) AS 'Total Produk' FROM tbproduk
 GROUP BY idkategori HAVING SUM(stok) >= 10 
+
+
+
+/* Tugas Praktikum 5*/
+-- No Tugas 1
+CREATE USER 'igo_tegar'@'localhost' IDENTIFIED BY '24sa11a159';
+
+-- No Tugas 2
+-- Cara 1
+GRANT SELECT ON	24sa11a159_ampu_sport.tbpenjualan TO 'igo_tegar'@'localhost';
+GRANT UPDATE (stok) ON 24sa11a159_ampu_sport.tbproduk TO 'igo_tegar'@'localhost';
+-- Cara 2
+GRANT SELECT, UPDATE (stok) ON 24sa11a159_ampu_sport.tbproduk TO 'igo_tegar'@'localhost';
+
+-- a. Melihat data produk dan penjualan
+GRANT SELECT ON	24sa11a159_ampu_sport.tbproduk TO 'igo_tegar'@'localhost';
+GRANT SELECT ON	24sa11a159_ampu_sport.tbpenjualan TO 'igo_tegar'@'localhost';
+
+-- b Update stok pada tabel produk
+GRANT UPDATE (stok) ON 24sa11a159_ampu_sport.tbproduk TO 'igo_tegar'@'localhost';
+
+-- c. Menambahkan data penjualan dan pembelian
+GRANT INSERT ON 24sa11a159_ampu_sport.tbpenjualan TO 'igo_tegar'@'localhost';
+GRANT INSERT ON 24sa11a159_ampu_sport.tbpembelian TO 'igo_tegar'@'localhost';
+
+REVOKE SELECT, UPDATE (stok) ON 24sa11a159_ampu_sport.tbproduk FROM 'igo_tegar'@'localhost';
+DROP USER 'igo_tegar'@'localhost'
+SHOW GRANTS FOR 'igo_tegar'@'localhost'
+
+-- No Tugas 3
+CREATE USER 'supervisor'@'localhost';
+GRANT ALL ON 24sa11a159_ampu_sport.* TO 'supervisor'@'localhost';
+
+-- No Tugas 4
+SELECT kode, nama, harga FROM tbproduk WHERE harga = (SELECT MAX(harga) FROM tbproduk)
+UNION
+SELECT kode, nama, harga FROM tbproduk WHERE harga = (SELECT MIN(harga) FROM tbproduk);
+
+-- No Tugas 5
+SELECT idkategori, AVG(harga) AS "Rata Rata Harga Produk" FROM tbproduk GROUP BY idkategori;
+
+-- No Tugas 6
+SELECT idpemasok, SUM(totalbeli) AS total_nilai_pembelian FROM tbpembelian GROUP BY idpemasok
+ORDER BY total_nilai_pembelian DESC;
+
+-- No Tugas 7
+SELECT tgltransaksi, SUM(total) AS total_penjualan
+FROM tbpenjualan GROUP BY tgltransaksi ORDER BY tgltransaksi ASC;
+
+-- No Tugas 8
+SELECT idmember, COUNT(nota) AS jumlah_transaksi
+FROM tbpenjualan GROUP BY idmember;
+
+-- No Tugas 9
+SELECT idpemasok, SUM(totalbeli) AS total_pembelian
+FROM tbpembelian GROUP BY idpemasok;
+
