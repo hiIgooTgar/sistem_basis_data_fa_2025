ab60472c759b74e5203c6236104c78f8fb84e696
hiIgooTgar <igotegarprambudhy128945@gmail.com>
2025-12-22 08:02:42 +0700

    Add module Cursor

--- praktikum_11/cursor.sql
+++ praktikum_11/cursor.sql
@@ -1,7 +1,194 @@
-/* Query cursor yang dapat menampung nama member domisili purwokerto */
+/*
+Query cursor yang dapat menampung nama member dari tabel member
+*/
 
 DELIMITER $$
-CREATE OR REPLACE PROCEDURE cursor_alamatmemmber()
+CREATE OR REPLACE PROCEDURE c_carimember(id VARCHAR(16))
+BEGIN 
+  DECLARE nama VARCHAR(64);
+  DECLARE cur1 CURSOR FOR
+	SELECT namamember FROM tbmember WHERE idmember = id;
+  DECLARE EXIT HANDLER FOR NOT FOUND
+  BEGIN 
+	SELECT CONCAT('Data member ', id, ' tidak ditemukan!') AS message;
+  END;
+  
+  OPEN cur1;
+  FETCH cur1 INTO nama;
+	SELECT nama;
+  CLOSE cur1;
+END $$
+
+CALL c_carimember('M02');
+CALL c_carimember('M10');
+
+
+
+
+
+/*
+Query cursor yang dapat menampung data member dikelompokkan berdasarkan alamat
+*/
+
+DELIMITER $$
+CREATE OR REPLACE PROCEDURE c_alamatcount()
+BEGIN
+ DECLARE p_alamat VARCHAR(64);
+ DECLARE p_count INT;
+ DECLARE selesai INT DEFAULT 0;
+ 
+ DECLARE cur1 CURSOR FOR
+	SELECT alamat, COUNT(*) FROM tbmember GROUP BY alamat;
+	
+ DECLARE CONTINUE HANDLER FOR NOT FOUND SET selesai = 1;
+ 
+ CREATE TEMPORARY TABLE IF NOT EXISTS tmp_alamat (
+	alamat VARCHAR(64),
+	jml_member INT
+ );
+ 
+ TRUNCATE tmp_alamat;
+ 
+ OPEN cur1;
+ ulang: LOOP
+	FETCH cur1 INTO p_alamat, p_count;
+	IF selesai = 1 THEN 
+		LEAVE ulang;
+	END IF;
+	INSERT INTO tmp_alamat VALUES (p_alamat, p_count);
+ END LOOP;
+ CLOSE cur1;
+ SELECT * FROM tmp_alamat;
+END $$
+
+CALL c_alamatcount();
+
+
+
+/* Query cursor yang dapat menampung data stok produk */
+
+DELIMITER $$
+CREATE OR REPLACE PROCEDURE cur_cekstok()
+BEGIN
+ DECLARE vnama VARCHAR(128);
+ DECLARE vstok INT;
+ DECLARE vket VARCHAR(64);
+ DECLARE selesai INT DEFAULT 0;
+ 
+ DECLARE c_stok CURSOR FOR 
+	SELECT nama, stok FROM tbproduk ORDER BY stok DESC;
+	
+ DECLARE CONTINUE HANDLER FOR NOT FOUND SET selesai = 1;
+ 
+ CREATE TEMPORARY TABLE IF NOT EXISTS tmp_stok (
+	nama_produk VARCHAR(128),
+	stok INT,
+	keterangan VARCHAR(64)
+ );
+ 
+ TRUNCATE tmp_stok;
+ 
+ OPEN c_stok;
+  ulang:LOOP
+    FETCH c_stok INTO vnama, vstok;
+    
+	IF selesai = 1 THEN
+		LEAVE ulang;
+	END IF;
+	
+	IF vstok = 0 THEN
+		SET vket = 'Habis';
+	ELSEIF vstok < 60 THEN 
+		SET vket = 'Menipis';
+	ELSE 
+		SET vket = 'Tersedia';
+	END IF;
+	
+	INSERT INTO tmp_stok VALUES (vnama, vstok, vket);
+  END LOOP;
+  CLOSE c_stok;
+  SELECT * FROM tmp_stok;
+END $$
+
+CALL cur_cekstok();
+
+
+
+
+
+
+
+/* Query cursor yang dapat menampung daftar produk dengan harga tertentu */
+
+DELIMITER $$
+CREATE OR REPLACE PROCEDURE cur_hargaproduk(IN p_harga INT)
+BEGIN
+ DECLARE vnama VARCHAR(128);
+ DECLARE vharga INT;
+ DECLARE selesai INT DEFAULT 0;
+ 
+ DECLARE c_produk CURSOR FOR
+	SELECT nama, harga FROM tbproduk WHERE harga >= p_harga;
+	
+ DECLARE CONTINUE HANDLER FOR NOT FOUND SET selesai = 1;
+ 
+ CREATE TEMPORARY TABLE IF NOT EXISTS tmp_produk (
+	nama_produk VARCHAR(128),
+	harga INT
+ );
+ 
+ TRUNCATE tmp_produk;
+
+ OPEN c_produk;
+ ulang: LOOP
+   FETCH c_produk INTO vnama, vharga;
+	
+	IF selesai = 1 THEN
+		LEAVE ulang;
+	END IF;
+   
+	INSERT INTO tmp_produk VALUES (vnama, vharga);
+ END LOOP;
+ CLOSE c_produk;
+ 
+ IF (SELECT COUNT(*) FROM tmp_produk) > 0 THEN
+	SELECT * FROM tmp_produk;
+ ELSE 
+	SELECT 'Data tidak ditemukan' AS message;
+ END IF;
+END $$
+
+
+CALL cursor_hargaproduk(200000);
+CALL cursor_hargaproduk(2000000);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+-- Soal Praktikum
+/* No. 1 Buat procedure menggunakan query cursor yang dapat menampung nama member
+yang beralamat Purwokerto. */
+
+DELIMITER $$
+CREATE OR REPLACE PROCEDURE cursor_alamatmember()
 BEGIN 
  DECLARE name_member VARCHAR(64);
  DECLARE exit_loop BOOLEAN;
@@ -23,7 +210,112 @@ BEGIN
 END LOOP ulang;
 END $$
 
-CALL cursor_alamatmemmber();
+CALL cursor_alamatmember();
+
+
+
+
+/* No. 2 Buatlah procedure menggunakan cursor untuk menghitung jumlah produk
+berdasarkan kategori [data menampilkan: idkategori, namakategori dan jumlah
+produk]. */ 
+
+DELIMITER $$
+
+CREATE PROCEDURE OR REPLACE cur_jmlperkategori()
+BEGIN
+    DECLARE v_idkategori VARCHAR(16);
+    DECLARE v_namakategori VARCHAR(128);
+    DECLARE v_jumlah INT;
+    DECLARE selesai INT DEFAULT 0;
+
+    DECLARE cur_kategori CURSOR FOR 
+        SELECT idkategori, nama_kategori FROM tbkategori;
+
+    DECLARE CONTINUE HANDLER FOR NOT FOUND SET selesai = 1;
+
+    CREATE TEMPORARY TABLE IF NOT EXISTS tmp_kategori (
+        idkategori VARCHAR(16),
+        nama_kategori VARCHAR(128),
+        jumlah INT
+    );
+    
+    TRUNCATE TABLE tmp_kategori;
+
+    OPEN cur_kategori;
+
+    ulang: LOOP
+        FETCH cur_kategori INTO v_idkategori, v_namakategori;
+        
+        IF selesai = 1 THEN 
+            LEAVE ulang;
+        END IF;
+        
+        SELECT COUNT(*) INTO v_jumlah FROM tbproduk WHERE idkategori = v_idkategori;
+        INSERT INTO tmp_kategori VALUES (v_idkategori, v_namakategori, v_jumlah);
+    END LOOP ulang;
+
+    CLOSE cur_kategori;
+
+    SELECT * FROM tmp_kategori;
+END $$
+
+CALL cur_jmlperkategori();
+
+
+
+
+/* Mo.3 Buatlah procedure cursor yang dapat menerima input kode produk dan menampilkan
+jumlah yang terjual. Jika kode produk tidak ada di tabel detail jual, tampilkan pesan
+“produk belum pernah terjual” */
+
+DELIMITER $$
+CREATE OR REPLACE PROCEDURE cur_jumlahterjual(IN p_kode VARCHAR(15))
+BEGIN
+    DECLARE v_jml INT;
+    DECLARE v_total_terjual INT DEFAULT 0;
+    DECLARE selesai INT DEFAULT 0;
+    DECLARE cek_data INT DEFAULT 0;
+
+    DECLARE cursor_jumlahjual CURSOR FOR 
+        SELECT jml FROM tbdetailjual WHERE kode = p_kode;
+
+    DECLARE CONTINUE HANDLER FOR NOT FOUND SET selesai = 1;
+
+    OPEN cursor_jumlahjual;
+
+    ulang: LOOP
+        FETCH cursor_jumlahjual INTO v_jml;
+        
+        IF selesai = 1 THEN 
+            LEAVE ulang;
+        END IF;
+
+        SET cek_data = 1;
+        
+        SET v_total_terjual = v_total_terjual + v_jml;
+    END LOOP ulang;
+
+    CLOSE cursor_jumlahjual;
+
+    IF cek_data = 1 THEN
+        SELECT p_kode AS kode, v_total_terjual AS jumlah_terjual;
+    ELSE
+        SELECT 'Produk belum pernah terjual' AS message;
+    END IF;
+
+END $$
+
+
+SELECT * FROM tbdetailjual;
+CALL cur_jumlahterjual('KPD02');
+CALL cur_jumlahterjual('KPD1-0');
+
+
+
+
+
+
+
 
 
 
@@ -31,7 +323,7 @@ CALL cursor_alamatmemmber();
 
 
 /*
-Query cursor yang dapat menampung nama member dari tabel pelanggan
+Query cursor yang dapat menampung nama member dari tabel member
 */
 
 DELIMITER $$
@@ -56,10 +348,6 @@ CALL cursor_carimember('M10');
 
 
 
-
-
-
-
 /*
 Query cursor yang dapat menampung jumlah member dikelompokkan berdasarkan alamat
 */
@@ -78,11 +366,6 @@ BEGIN
 END $$
 
 CALL cursor_membercount();
-
-
-
-
-
 
 
 /*
@@ -120,10 +403,6 @@ SELECT * FROM tbproduk ORDER BY harga DESC;
 
 
 
-
-
-
-
 /*
 Jika daftar produk di tabel produk diurutkan secara menaik (ascending) berdasarkan
 stok, perintah query cursor yang dapat menampilkan daftar 5 produk teratas yang ada
@@ -153,3 +432,19 @@ BEGIN
 END $$
 
 CALL cursor_stokbarang();
+
+
+
+DROP PROCEDURE c_carimember;
+DROP PROCEDURE c_alamatcount;
+DROP PROCEDURE cur_cekstok;
+DROP PROCEDURE cur_hargaproduk;
+
+DROP PROCEDURE cursor_alamatmember;
+DROP PROCEDURE cur_jmlperkategori;
+DROP PROCEDURE cur_jumlahterjual;
+ 
+DROP PROCEDURE cursor_carimember;
+DROP PROCEDURE cursor_hargaproduk;
+DROP PROCEDURE cursor_membercount;
+DROP PROCEDURE cursor_stokbarang;
